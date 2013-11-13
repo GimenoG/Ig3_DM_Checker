c7cc9b490ff0d6d8064ddbb95686770c8065eaa7
Meriadoc <Meriadoc@172.22.152.214>
2013-11-13 17:25:43 +0100

    test

--- DM_Checker/src/main/main.java
+++ DM_Checker/src/main/main.java
@@ -8,5 +8,6 @@ public class main {
 		System.out.println("tu suxx noob");
 		System.out.println("https://github.com/Guiiiii/Ig3_DM_Checker.git: git-receive-pack not permitted");
 		System.out.println("Hoo le beau test");
+		System.out.println("tu veux voire ma bite ?");
 	}
 }
