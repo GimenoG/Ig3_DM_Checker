b5f3169f2ca63c3e6b3bc8bcd0360dd4b83508bb
Guiiiii <gimeno.g@gmail.com>
2013-11-13 17:26:37 +0100

    Let's go !!!

--- DM_Checker/src/main/main.java
+++ DM_Checker/src/main/main.java
@@ -3,11 +3,6 @@ package main;
 public class main {
 
 	public main() {
-		System.out.println("plop le menu principal");
-		System.out.println("toto");
-		System.out.println("tu suxx noob");
-		System.out.println("https://github.com/Guiiiii/Ig3_DM_Checker.git: git-receive-pack not permitted");
-		System.out.println("Hoo le beau test");
-		System.out.println("tu veux voire ma bite ?");
+
 	}
 }
