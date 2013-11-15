0e4c4a811d597a1afa0bcc69c394bb4b9ebfd97d
Guiiiii <gimeno.g@gmail.com>
2013-11-16 00:04:56 +0100

    coucou

--- DM_Checker/src/fr/umlv/zip/ReadingArchive.java
+++ DM_Checker/src/fr/umlv/zip/ReadingArchive.java
@@ -1,77 +1,92 @@
 package fr.umlv.zip;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 public class ReadingArchive {
-	/**
-     * décompresse le fichier zip dans le répertoire donné
-     * @param folder le répertoire où les fichiers seront extraits
-     * @param zipfile le fichier zip à décompresser
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-	 public static void extractFolder(String zipFile) throws ZipException, IOException 
-	{
-	    System.out.println(zipFile);
-	    int BUFFER = 2048;
-	    File file = new File(zipFile);
+	/*
+	 * public static void extractFolder(String zipFile) throws ZipException,
+	 * IOException { System.out.println(zipFile); int BUFFER = 2048; File file =
+	 * new File(zipFile); System.out.println(file); ZipFile zip = new
+	 * ZipFile(file); System.out.println(zip); String newPath =
+	 * zipFile.substring(0, zipFile.length() - 4); System.out.println(newPath);
+	 * 
+	 * new File(newPath).mkdir(); Enumeration zipFileEntries = zip.entries();
+	 * System.out.println("avant while"); // Process each entry while
+	 * (zipFileEntries.hasMoreElements()) { // grab a zip file entry ZipEntry
+	 * entry = (ZipEntry) zipFileEntries.nextElement(); String currentEntry =
+	 * entry.getName(); File destFile = new File(newPath, currentEntry);
+	 * //destFile = new File(newPath, destFile.getName()); File
+	 * destinationParent = destFile.getParentFile();
+	 * 
+	 * // create the parent directory structure if needed
+	 * destinationParent.mkdirs(); System.out.println("avant if"); if
+	 * (!entry.isDirectory()) { BufferedInputStream is = new
+	 * BufferedInputStream(zip .getInputStream(entry)); int currentByte; //
+	 * establish buffer for writing file byte data[] = new byte[BUFFER];
+	 * 
+	 * // write the current file to disk FileOutputStream fos = new
+	 * FileOutputStream(destFile); BufferedOutputStream dest = new
+	 * BufferedOutputStream(fos, BUFFER);
+	 * 
+	 * // read and write until last byte is encountered while ((currentByte =
+	 * is.read(data, 0, BUFFER)) != -1) { dest.write(data, 0, currentByte); }
+	 * dest.flush(); dest.close(); is.close(); }
+	 * 
+	 * if (currentEntry.endsWith(".zip")) { // found a zip file, try to open
+	 * extractFolder(destFile.getAbsolutePath()); } } }
+	 */
 
-	    ZipFile zip = new ZipFile(file);
-	    String newPath = zipFile.substring(0, zipFile.length() - 4);
+	// buffer to hold contents
 
-	    new File(newPath).mkdir();
-	    Enumeration zipFileEntries = zip.entries();
+	//	/Users/Gui/Documents/Lambda/loginnam12.zip
+	//	/Users/Gui/Documents/Lambda/Archive2.zip
 
-	    // Process each entry
-	    while (zipFileEntries.hasMoreElements())
-	    {
-	        // grab a zip file entry
-	        ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
-	        String currentEntry = entry.getName();
-	        File destFile = new File(newPath, currentEntry);
-	        //destFile = new File(newPath, destFile.getName());
-	        File destinationParent = destFile.getParentFile();
+	public static void unzip(ZipFile zipFile, File unzipDir) {
+		Enumeration<? extends ZipEntry> files = zipFile.entries();
+		File f = null;
+		FileOutputStream fos = null;
 
-	        // create the parent directory structure if needed
-	        destinationParent.mkdirs();
+		while (files.hasMoreElements()) {
+			try {
+				ZipEntry entry = (ZipEntry) files.nextElement();
+				InputStream eis = zipFile.getInputStream(entry);
+				byte[] buffer = new byte[1024];
+				int bytesRead = 0;
 
-	        if (!entry.isDirectory())
-	        {
-	            BufferedInputStream is = new BufferedInputStream(zip
-	            .getInputStream(entry));
-	            int currentByte;
-	            // establish buffer for writing file
-	            byte data[] = new byte[BUFFER];
+				f = new File(unzipDir.getAbsolutePath() + File.separator
+						+ entry.getName());
 
-	            // write the current file to disk
-	            FileOutputStream fos = new FileOutputStream(destFile);
-	            BufferedOutputStream dest = new BufferedOutputStream(fos,
-	            BUFFER);
+				if (entry.isDirectory()) {
+					f.mkdirs();
+					continue;
+				} else {
+					f.getParentFile().mkdirs();
+					f.createNewFile();
+				}
 
-	            // read and write until last byte is encountered
-	            while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
-	                dest.write(data, 0, currentByte);
-	            }
-	            dest.flush();
-	            dest.close();
-	            is.close();
-	        }
+				fos = new FileOutputStream(f);
 
-	        if (currentEntry.endsWith(".zip"))
-	        {
-	            // found a zip file, try to open
-	            extractFolder(destFile.getAbsolutePath());
-	        }
-	    }
+				while ((bytesRead = eis.read(buffer)) != -1) {
+					fos.write(buffer, 0, bytesRead);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			} finally {
+				if (fos != null) {
+					try {
+						fos.close();
+					} catch (IOException e) {
+						// Log
+					}
+				}
+			}
+		}
 	}
-
 }
--- DM_Checker/src/main/Main.java
+++ DM_Checker/src/main/Main.java
@@ -3,18 +3,20 @@ package main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.zip.ZipFile;
 
 import fr.umlv.zip.ReadingArchive;
+import fr.umlv.zip.ok;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-
-		    String zipfile =  args[0];
-		    
-
-		    ReadingArchive.extractFolder(zipfile);
-		
-		}
+	//	/Users/Gui/Documents/Lambda/loginnam12.zip
+	//	/Users/Gui/Documents/Lambda/Archive2.zip
+        //fetch command line argument
+		ZipFile zipFile = new ZipFile("/Users/Gui/Documents/Lambda/plusieursautop.zip");
+		File unzipDir = new File("/Users/Gui/Documents/Lambda/plop");
+		ok.unzip("/Users/Gui/Documents/Lambda/plusieursautop.zip");
 		
 	}
+}
