952114fbc44652014f431fd4ef93b0e01db042cd
Meriadoc <Meriadoc@Meriadoc-Port>
2013-11-16 01:03:44 +0100

    modif main

--- DM_Checker/src/main/Main.java
+++ DM_Checker/src/main/Main.java
@@ -3,11 +3,16 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import com.martiansoftware.jsap.JSAPException;
+
+import fr.umlv.options.Options;
 import fr.umlv.zip.ReadingArchive;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException, JSAPException {
+		Options.createOptions();
+		Options.checkOptions(args);
 		// /Users/Gui/Documents/Lambda/loginnam12.zip
 		// /Users/Gui/Documents/Lambda/Archive2.zip
 		ReadingArchive.unzip("/Users/Gui/Documents/Lambda/Archive2.zip");		
