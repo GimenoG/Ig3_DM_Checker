ebccda8ae0c2e3b6a53cc1dbda43a7325f7f35d8
Meriadoc <Meriadoc@Meriadoc-Port>
2013-11-16 10:51:39 +0100

    structure des test d'option dans options

--- DM_Checker/src/fr/umlv/options/Options.java
+++ DM_Checker/src/fr/umlv/options/Options.java
@@ -9,6 +9,7 @@ import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.QualifiedSwitch;
 import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.UnflaggedOption;
 
 import fr.umlv.zip.ReadingArchive;
 
@@ -38,7 +39,11 @@ public class Options {
 	 */
 	
 	public static void createOptions() throws JSAPException{
+		//TODO option forcé ?
+		
 		jsap = new JSAP();
+		
+		UnflaggedOption path = new UnflaggedOption("path").setStringParser(JSAP.STRING_PARSER).setDefault("World").setRequired(true).setGreedy(true);
 		
 		FlaggedOption opt1 = (FlaggedOption) new FlaggedOption("count").setStringParser(JSAP.INTEGER_PARSER).setDefault("-1").setRequired(true).setLongFlag(JSAP.NO_LONGFLAG).setShortFlag(JSAP.NO_SHORTFLAG).setHelp("Check only one archive.");
 		FlaggedOption opt2 = (FlaggedOption) new FlaggedOption("count").setStringParser(JSAP.INTEGER_PARSER).setDefault("-2").setRequired(true).setLongFlag(JSAP.NO_LONGFLAG).setShortFlag(JSAP.NO_SHORTFLAG).setHelp("Check archives of archive");
@@ -49,11 +54,14 @@ public class Options {
 		//TODO help (a verif sens de l'option)
 		QualifiedSwitch optO = (QualifiedSwitch) (new QualifiedSwitch("optO").setShortFlag('o').setLongFlag("onetop").setList(true).setListSeparator(',').setHelp("."));
 		QualifiedSwitch optE = (QualifiedSwitch) (new QualifiedSwitch("optE").setShortFlag('e').setLongFlag("endswith").setList(true).setListSeparator(',').setHelp("Doesn't work with the files which end with the paramter : -e <end string to bansih> or --endswith <end string to banish>"));
-		QualifiedSwitch optB = (QualifiedSwitch) (new QualifiedSwitch("optB").setShortFlag('b').setLongFlag("beginswith").setList(true).setListSeparator(',').setHelp("Doesn't work with the files which start with the paramter : -e <start string to bansih> or --endswith <start string to banish>"));
+		QualifiedSwitch optB = (QualifiedSwitch) (new QualifiedSwitch("optB").setShortFlag('b').setLongFlag("beginswith").setList(true).setListSeparator(',').setHelp("Doesn't work with the files which start with the paramter : -b <start string to bansih> or --beginswith <start string to banish>"));
 		//TODO qualifiedSwith ou autre ?
 		QualifiedSwitch optX = (QualifiedSwitch) (new QualifiedSwitch("optX").setShortFlag('x').setLongFlag("existe").setList(true).setListSeparator(',').setHelp("Check if the folder name in parameter already existe in the destination directory. If it's true the folder won't be treaty"));
 		QualifiedSwitch optI = (QualifiedSwitch) (new QualifiedSwitch("optI").setShortFlag('i').setLongFlag("interdit").setList(true).setListSeparator(',').setHelp("Check the lack of the file named <param> in the repository. The head directory isn't concern by is option.\nUse : -I <regex name> or --interdit <regex name>"));
 		
+		jsap.registerParameter(path);
+		jsap.registerParameter(opt1);
+		jsap.registerParameter(opt2);
 		jsap.registerParameter(optD);
 		jsap.registerParameter(optV);
 		jsap.registerParameter(optO);
@@ -61,8 +69,6 @@ public class Options {
 		jsap.registerParameter(optB);
 		jsap.registerParameter(optX);
 		jsap.registerParameter(optI);
-		jsap.registerParameter(opt1);
-		jsap.registerParameter(opt2);
 	}
 	/**
 	 * 
@@ -93,20 +99,42 @@ public class Options {
 		}	
 		
 	}
-	
-	public static void Launch(Paths path){
+	/**
+	 * Launch the software with the rigth option.
+	 * 
+	 */
+	//TODO pattern ? Options porte aussi l'execution du prog :s mais je ne voi pas comment faire autrement
+	public static void Launch(){
+		ReadingArchive ra = new ReadingArchive(config.getString("path"));//ordre ?
+		String pathDestination; //gerer le -d et le param de fin
 		
-		ReadingArchive ra = new ReadingArchive();
+		//option commune a tout
+		if(config.getBoolean("optE")){
+			//modif du path (retire a la fin
+		}
+		if(config.getBoolean("optB")){
+			//modif du path (retire au debit
+		}
+		if(config.getBoolean("optV")){
+			//creation du logger ? -> ajout dans le bordel
+			//set bolleen dans readingarchive
+		}
+		//mode d'utilisation
+		if(config.getBoolean("optX")){
+			String [] fileToCheck = config.getStringArray("optX");
+			//parcours du dossier a la recherche du fichier / des fichiers
+			//affiche le resultat
+		}
+		else if(config.getBoolean("optI")){
+			String [] fileToCheck = config.getStringArray("optI");
+			//check la présence du fichier / des fichiers
+			//affiche le résultat
+		}
+		if(config.getBoolean("optO")){
+			//check format de la racine de l'archive
+		}
+		
 	}
-		/**
-		 * give the config of the options to test.
-		 */
-		
-	//TODO Heu static c'est de la merde en fait ?
-	/*public static JSAPResult getConfig(){
-		return this.config;
-	}*/
-	
 	
 	
 }
