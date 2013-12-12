10281969316a07414c307682fe2f38272d14c32f
Meriadoc <Meriadoc@Meriadoc-Port>
2013-12-12 17:10:35 +0100

    regex

--- DM_Checker/src/fr/umlv/util/Regex.java
+++ DM_Checker/src/fr/umlv/util/Regex.java
@@ -1,55 +1,47 @@
 package fr.umlv.util;
 
+
+
 import java.io.File;
-import java.net.IDN;
+
+
 
 public class Regex {
 
-	//A supprimer
-	public static boolean endsWith(String ends,String entryName){
-		return entryName.endsWith(ends);
-	}
-	
-	//A supprimer
-	public boolean startWith(String path,String start){
-		String file = new File("/Users/Gui/Documents/Lambda/Archive2.zip").getName();
-		return file.startsWith(start);
-	}
 
-	public static String changeURL(String s){
-		String result;
-		result = s.replace("\\" , "\\");
-		return result;
-	}
-	
-	public static String idName(String nameFile){
-		String[] id;
-		id=nameFile.split("_");
-		return id[0];
-	}
-	
-	public static String idKey(String nameFile){
-		String[] id;
-		id=nameFile.split("_");
-		return id[2].substring(0, id[2].length() - 4);
-	}
-	
-	public static String nameFolder(String s){
-		String[] id;
-		id=s.split("/");
-		return id[id.length-2];
-	}
-	
-	public static void main(String[] args) {
-//		String file = "NOM PRENOMS_nomdonneparletudiantasonrendu_00000.zip";
-//		System.out.println(idName(file));
-//		System.out.println(substitute(idName(file)));
-		//System.out.println(endsWith("cou", "coucou"));
-		String file = "/Users/Gui/Documents/Lambda/lezipdezip/ HANY WUTHEARA_hawu_projet_9093";
-		System.out.println(nameFolder(file));
-	}
-	
-	public static String substitute(String s){
-		return s.replaceAll("\\s","_");
-	}
+
+public static String idName(String nameFile) {
+
+String[] id = nameFile.split("_");
+
+String[] id2;
+
+id2 = id[0].split(File.separator);
+
+return id2[id2.length - 1];
+
+}
+
+
+
+public static String idKey(String nameFile) {
+
+String[] id = nameFile.split("_");
+
+return id[id.length - 1].substring(0, id[id.length - 1].length() - 4);
+
+}
+
+
+
+public static String nameFolder(String s) {
+
+String[] id = s.split("/");
+
+return id[id.length - 2];
+
+}
+
+
+
 }
